8c65648fe26fa7eabf1921f43ca57a291148a51f
Marko <mfrankovi@student.unipu.hr>
2022-04-18 13:02:50 +0200

    Login screen

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Autetifikacija</router-link>|
+      <router-link to="/about">Pičenator</router-link>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #8d42b9;
+}
+</style>
---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="simple_button font-weight-bold display-1"
+    @click="redirect(cards[0].route, true)"
+  ></button>
+</template>
+
+<style>
+.simple_button {
+  background-color: white;
+  background-image: url(C:\Users\Marko\Documents\GitHub\FrontEnd-Picenator3000\frontend\src\assets\merlot.jpg);
+  height: 200px;
+  width: 200px;
+  border: none;
+  border-radius: 50%;
+  padding: 0px;
+  text-align: center;
+  display: inline-block;
+  outline: none;
+  border: 0;
+}
+</style>
