cd54c019a493e789a27832338bbc0a9afc763f7e
Marko <mfrankovi@student.unipu.hr>
2022-04-29 19:06:30 +0200

    Update AboutView.vue

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -7,32 +7,33 @@
           src="C:\Users\Marko\Documents\GitHub\FrontEnd-Picenator3000\frontend\src\assets\stock.jpgg"
         />
         <button v-on:click="prebacivanje">Button 1</button>
-        <button>Button 2</button>
-        <button>Button 3</button>
+        <button v-on:click="prebacivanje">Button 2</button>
+        <button v-on:click="prebacivanje">Button 3</button>
       </button-group>
     </div>
     <div>
       <button-group>
-        <button>Button 4</button>
-        <button>Button 5</button>
-        <button>Button 6</button>
+        <button v-on:click="prebacivanje">Button 4</button>
+        <button v-on:click="prebacivanje">Button 5</button>
+        <button v-on:click="prebacivanje">Button 6</button>
       </button-group>
     </div>
     <div>
       <button-group>
-        <button>Button 7</button>
-        <button>Button 8</button>
-        <button>Button 9</button>
+        <button v-on:click="prebacivanje">Button 7</button>
+        <button v-on:click="prebacivanje">Button 8</button>
+        <button v-on:click="prebacivanje">Button 9</button>
       </button-group>
     </div>
   </div>
 </template>
 
 <script>
-methods: {
-  prebacivanje();
-  {
-    this.$router.push("/player");
-  }
-}
+export default {
+  methods: {
+    prebacivanje() {
+      this.$router.push("/player");
+    },
+  },
+};
 </script>
