08e1cdd5c16ca9478e4bb95a83ab66275b4f91b4
Marko <mfrankovi@student.unipu.hr>
2022-05-03 17:29:19 +0200

    Update

--- frontend/src/views/PjesmeView.vue
+++ frontend/src/views/PjesmeView.vue
@@ -3,7 +3,7 @@
     <iframe
       width="560"
       height="315"
-      src="https://www.youtube.com/embed/3wnb054c5l0"
+      src="https://www.youtube.com/embed/g5CHYGRAMbA"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
