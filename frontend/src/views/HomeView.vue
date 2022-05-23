a0e3e3d08099dc9ae142525d75a0bb2514a809ba
Marko <mfrankovi@student.unipu.hr>
2022-05-23 12:37:50 +0200

    Update HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -8,7 +8,6 @@
           <div class="w3-display-middle">
             <h1 class="w3-jumbo w3-animate-top">Pičenator 3000</h1>
             <hr class="w3-border-grey" style="margin: auto; width: 20%" />
-            <p class="w3-large w3-center">Moraš popit sve...</p>
           </div>
 
           <i>
@@ -68,30 +67,17 @@ hr {
   margin: auto;
 }
 
-p {
-  font-family: "Raleway", sans-serif;
-  color: whitesmoke;
-  outline-color: grey;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  top: auto;
-  margin: auto;
-  text-align: center;
-}
-
 i {
   font-family: "Raleway", sans-serif;
   color: whitesmoke;
   outline-color: grey;
+  height: 0.1px;
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   margin: auto;
-  text-align: center;
 }
 
 body,
@@ -102,9 +88,10 @@ html {
 .bgimg {
   background-image: url("@/assets/img/thumbnail.jpg");
   background-position: center;
-  background-size: auto;
+  background-size: 1000px;
   background-position: center center;
-  width: 3000px;
+  background-repeat: no-repeat;
+  width: 950px;
   height: 950px;
 }
 </style>
