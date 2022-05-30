c9f5db687ec26adb75fbfe1a1c3ccb0575d6a946
Marko <mfrankovi@student.unipu.hr>
2022-05-30 23:38:41 +0200

    Update HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="container">
-    <div class="centered">
-      <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
-        <div class="w3-display-middle">
-          <h1 class="w3-jumbo w3-animate-top">Pičenator 3000</h1>
-          <hr class="w3-border-grey" style="margin: auto; width: 20%" />
-        </div>
-
+  <div>
+    <div>
+      <div>
         <i>
-          <div class="col d-flex justify-content-center">
+          <div>
+            <div>
+              <h1>Pičenator 3000</h1>
+              <hr class="w3-border-grey" style="margin: auto; width: 100%" />
+            </div>
             <button
               v-on:click="korisnik"
               type="button"
@@ -60,34 +59,10 @@ body,
   align-items: center;
 }
 
-h1 {
-  font-family: "Raleway", gelve;
-  color: whitesmoke;
-  outline-color: rgb(92, 92, 92);
-  width: 1000px;
-  height: 100px;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-  text-align: center;
-}
-
-hr {
-  color: whitesmoke;
-  outline-color: rgb(92, 92, 92);
-  width: 50%;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-}
-
 i {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-family: "Raleway", sans-serif;
   color: whitesmoke;
   outline-color: rgb(92, 92, 92);
@@ -98,19 +73,16 @@ i {
   left: 0;
   right: 0;
   margin: auto;
+  background-image: url("@/assets/img/thumbnail.jpg");
+  background-repeat: no-repeat;
+  background-size: 100%;
+  width: 100%;
+  height: 100%;
 }
 
 body,
 html {
   min-height: 100%;
   min-width: 100%;
-}
-
-body {
-  background-image: url("@/assets/img/thumbnail.jpg");
-  background-repeat: no-repeat;
-  background-size: 100%;
-  width: 100%;
-  height: 100%;
 }
 </style>
