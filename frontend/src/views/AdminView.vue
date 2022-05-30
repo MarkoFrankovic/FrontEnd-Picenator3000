e67d9b6b1d64918b516c25931bb3121b8ae0fc00
Marko <mfrankovi@student.unipu.hr>
2022-05-30 12:02:47 +0200

    Update AdminView.vue

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,53 +1,55 @@
 <template>
-  <div class="container">
-    <span class="input-group-text">Izvođač i ime pjesme:</span>
-    <input
-      type="text"
-      class="form-control"
-      placeholder=""
-      aria-label="Example text with button addon"
-      aria-describedby="button-addon1"
-      v-model="izvodac_ime_pjesme"
-    />
-    <span class="input-group-text">Url:</span>
-    <input
-      type="text"
-      class="form-control"
-      placeholder=""
-      aria-label="Example text with button addon"
-      aria-describedby="button-addon1"
-      v-model="url"
-    />
-    <span class="input-group-text">Ocjena:</span>
-    <input
-      type="text"
-      class="form-control"
-      placeholder=""
-      aria-label="Example text with button addon"
-      aria-describedby="button-addon1"
-      v-model="ocjena"
-    />
-    <span class="input-group-text">Piće:</span>
-    <input
-      type="text"
-      class="form-control"
-      placeholder=""
-      aria-label="Example text with button addon"
-      aria-describedby="button-addon1"
-      v-model="pice"
-    />
-    <button
-      v-on:click="posalji()"
-      type="button"
-      class="btn btn-dark"
-      style="margin: 1em"
-    >
-      Submit
-    </button>
-    <div>
-      <button v-on:click="natrag" class="btn btn-dark" style="margin: 1em">
-        Natrag
+  <div>
+    <div class="container">
+      <span class="input-group-text">Izvođač i ime pjesme:</span>
+      <input
+        type="text"
+        class="form-control"
+        placeholder=""
+        aria-label="Example text with button addon"
+        aria-describedby="button-addon1"
+        v-model="izvodac_ime_pjesme"
+      />
+      <span class="input-group-text">Url:</span>
+      <input
+        type="text"
+        class="form-control"
+        placeholder=""
+        aria-label="Example text with button addon"
+        aria-describedby="button-addon1"
+        v-model="url"
+      />
+      <span class="input-group-text">Ocjena:</span>
+      <input
+        type="text"
+        class="form-control"
+        placeholder=""
+        aria-label="Example text with button addon"
+        aria-describedby="button-addon1"
+        v-model="ocjena"
+      />
+      <span class="input-group-text">Piće:</span>
+      <input
+        type="text"
+        class="form-control"
+        placeholder=""
+        aria-label="Example text with button addon"
+        aria-describedby="button-addon1"
+        v-model="pice"
+      />
+      <button
+        v-on:click="posalji()"
+        type="button"
+        class="btn btn-dark"
+        style="margin: 1em"
+      >
+        Submit
       </button>
+      <div>
+        <button v-on:click="natrag" class="btn btn-dark" style="margin: 1em">
+          Natrag
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -101,3 +103,21 @@ export default {
   },
 };
 </script>
+<style>
+.form-control {
+  width: 100%;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+</style>
