a3f5e0c7242938af30d5cdf97b4de8c19dc0cde7
Marko <mfrankovi@student.unipu.hr>
2022-06-22 19:57:13 +0200

    Update SpecificView.vue

--- Frontend/src/views/SpecificView.vue
+++ Frontend/src/views/SpecificView.vue
@@ -5,7 +5,7 @@
       <div class="col d-flex justify-content-center">
         <div class="card">
           <li>
-            {{ podatci.ime }}
+            {{ podatci }}
           </li>
         </div>
       </div>
